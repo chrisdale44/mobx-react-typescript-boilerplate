32b7cacc021d6df0ad4d93e35c5a5a67666f7fb1
chrisdale44 <Chris@Chriss-Air.lan>
2016-09-15 01:31:09 +0100

    localhost must be https for pinterest API

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
+  https: true,
   publicPath: config.output.publicPath,
   hot: true,
+  watch:true,
   historyApiFallback: true
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at localhost:3000');
+  console.log('Listening at https://localhost:3000');
 });
