fc8478fbd49dce9728304a3800205a215cf4ea53
chrisdale44 <Chris@Chriss-Air.lan>
2016-09-15 13:34:33 +0100

    add board pins request

--- src/util/const.ts
+++ src/util/const.ts
@@ -5,7 +5,8 @@ var redirect = window.location.origin + window.location.pathname;
 
 const PIN = {
     APP: '4856836359995802992', //4855819497409359779
-    FIELDS: 'id,name,image[small]',
+    BOARD_FIELDS: 'id,name,image[small]',
+    PIN_FIELDS: 'id,board,note,color,media,attribution,image[small],metadata',  
     SCOPE: 'read_public, write_public'
 };
 
--- src/util/pinterest.ts
+++ src/util/pinterest.ts
@@ -34,15 +34,23 @@ var Pinterest = {
      *  @param {Function} callback - function fired on completion
      */
     myBoards: function(callback) {
-        PDK.me('boards', { fields: PIN.FIELDS }, callback);
+        PDK.me('boards', { fields: PIN.BOARD_FIELDS, limit: 100 }, callback);
     },
     /*
-     *  Use SDK to request current pins for a board
+     *  Use SDK to request all pins (limit 100)
      *  @param
      *  @param {Function} callback - function fired on completion
      */
     myPins: function(callback) {
-        PDK.me('pins', { fields: PIN.FIELDS }, callback);
+        PDK.me('pins', { fields: PIN.PIN_FIELDS, limit: 100 }, callback);
+    },
+    /*
+     *  Use SDK to request pins for a board (limit 100)
+     *  @param
+     *  @param {Function} callback - function fired on completion
+     */
+    myBoardPins: function(boardId, callback) {
+        PDK.request(`/boards/${boardId}`, {fields: PIN.PIN_FIELDS, limit: 100 }, callback);
     }
 };
 
