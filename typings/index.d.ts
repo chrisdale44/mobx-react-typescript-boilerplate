fc06c47b826c8215aac9a1e2428bb7c5ad1f2876
chrisdale44 <Chris@Chriss-Air.lan>
2016-09-16 00:20:40 +0100

    add lodash typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="../interfaces/index.d.ts" />
 /// <reference path="globals/require/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
 /// <reference path="modules/react-dom/index.d.ts" />
 /// <reference path="modules/react-router/index.d.ts" />
 /// <reference path="modules/react/index.d.ts" />
