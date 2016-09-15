f766e1d2eee10a333f2a57c5d5c08e9f782c2cbe
chrisdale44 <Chris@Chriss-Air.lan>
2016-09-15 19:04:33 +0100

    update pin API request

--- src/util/const.ts
+++ src/util/const.ts
@@ -4,9 +4,9 @@
 var redirect = window.location.origin + window.location.pathname;
 
 const PIN = {
-    APP: '4856836359995802992', //4855819497409359779
-    BOARD_FIELDS: 'id,name,image[large]',
-    PIN_FIELDS: 'id,board,note,color,media,attribution,image[small],metadata',  
+    APP: '4856836359995802992',
+    BOARD_FIELDS: 'id,name,image[large],creator',
+    PIN_FIELDS: 'id,board,note,color,media,attribution,image[large],metadata',  
     SCOPE: 'read_public, write_public'
 };
 
