7124b6d6571a8c983ffbc5901549df124c15e8d3
chrisdale44 <Chris@Chriss-Air.lan>
2016-09-15 13:31:55 +0100

    move view components

--- src/views/Pins.tsx
+++ src/views/Pins.tsx
@@ -1,22 +1,26 @@
 import * as React from 'react';
 import pinterest from '../util/pinterest';
 
+interface IProps {
+    boardId: string;
+}
+
 interface IState {
     pins?: {}
 }
 
 // does this need to be wrapped in withRouter?
-class Pins extends React.Component<any, IState> {
+class Pins extends React.Component<IProps, IState> {
   componentDidMount() {
-      this.fetchBoardData();
+      this.fetchBoardData(this.props.boardId);
   }
 
   /*
   *  Fetch user boards from Pinterest
   */
-  fetchBoardData() {
-      pinterest.myPins(response => {
-          this.setState({ pins: response.data.sort((a,b) => a.name > b.name ? 1 : -1) });
+  fetchBoardData(boardId) {
+      pinterest.myBoardPins(boardId, response => {
+          this.setState({ pins: response.data });
       });
   }
 
