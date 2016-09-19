5728cb915f55ef539823dad43746beb4176a2747
christopherdale <christopher.j.dale@baesystems.com>
2016-09-19 17:54:21 +0100

    large images

--- src/util/const.ts
+++ src/util/const.ts
@@ -5,8 +5,8 @@ var redirect = window.location.origin + window.location.pathname;
 
 const PIN = {
     APP: '4856836359995802992',
-    BOARD_FIELDS: 'id,name,image[small],creator',
-    PIN_FIELDS: 'id,board,note,color,media,attribution,image[small],metadata',  
+    BOARD_FIELDS: 'id,name,image[large],creator',
+    PIN_FIELDS: 'id,board,note,color,media,attribution,image[large],metadata',  
     SCOPE: 'read_public, write_public'
 };
 
